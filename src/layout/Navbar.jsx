2f671760a620783169963687c93cd46051b5804e
Muhammad Ullah <muhullah25@gmail.com>
2026-01-05 09:32:35 -0800

    fini nav

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -5,20 +5,18 @@ const navLinks = [
     {href: "experience", label: "Experience"},
 ]
 
-
-
 export const Navbar = () => {
     return(
-        <header>
+        <header class="fixed top-0 left-0 right-0 py-5">
             {/* inside the header will be a nav, will contain 2 things, initials and links */}
-            <nav> 
-                <a> 
-                    MU <span>.</span>
+            <nav class="mx-auto px-6 flex justify-between"> 
+                <a href ="#" class="font-bold tracking-tight hover:text-primary"> 
+                    MU<span>.</span>
                 </a>
-                <div>
-                    <div>
-
-                    </div>
+                <div class ="hidden md:flex navigate flex rounded-full gap-2 px-2 py-1 items-center">
+                    {navLinks.map((nav) => (
+                        <a href = {nav.href} class ="text-sm px-3 py-1 hover:text-primary">{nav.label}</a>
+                    ))}
                 </div>
             </nav>
         </header>
