19b5f27a1554fbe21ff75efeb525b062fc1fca61
Muhammad Ullah <muhullah25@gmail.com>
2026-01-06 13:04:08 -0800

    bordering visualization

--- src/layout/Navbar.jsx
+++ src/layout/Navbar.jsx
@@ -1,3 +1,6 @@
+import { Menu, X } from "lucide-react"
+import { useState } from "react"
+
 /*objects of navigation links */
 const navLinks = [
     {href: "#about", label: "About"},
@@ -6,6 +9,8 @@ const navLinks = [
 ]
 
 export const Navbar = () => {
+    
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);    
     return(
         <header class="fixed top-0 left-0 right-0 py-5">
             {/* inside the header will be a nav, will contain 2 things, initials and links */}
@@ -18,7 +23,19 @@ export const Navbar = () => {
                         <a href = {nav.href} class ="text-sm px-3 py-1 hover:text-primary">{nav.label}</a>
                     ))}
                 </div>
+                {/*mobile menu button */}
+                <button class="md:hidden test-foreground" onClick={() => setIsMobileMenuOpen((prev) => !prev)}>
+                    {isMobileMenuOpen ? <X /> : <Menu />}
+                </button>
             </nav>
+            {/*hidden menu */}
+            {isMobileMenuOpen && (
+                <div class="md:hidden navigate mx-auto px-6 py-6 flex flex-col gap-4">{navLinks.map((nav) => (
+                    <a href = {nav.href} class ="text-base px-3 py-1 hover:text-primary">{nav.label}</a>
+                ))}</div>
+            )}
+            
+
         </header>
     );
 }
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,34 @@
 export const Hero = () => {
-    return <section>
+    return <section class="min-h-screen flex items-center overflow-hidden pt-20 ">
+        <div class="border-2 border-yellow-300 w-full max-w-5xl p-10 grid grid-cols-1 md:grid-cols-2 gap-6 mx-auto">
+            {/*left column*/}
+            <div class ="p-4 border-2 border-yellow-300 items-center">
+                {/*Title*/}
+                <div class="border-2 border-yellow-300 h-10 mb-2 px-2">
+        
+                </div>
+                {/*Name*/}
+                <div class="border-2 border-yellow-300 h-16 mb-2 px-2 text-2xl font-bold">
+
+                </div>
+                {/*Description*/}
+                <div>
+
+                </div>
+                {/*Download CV*/}
+                <div>
+
+                </div>
+                {/*Links*/}
+                <div>
+
+                </div>
+            </div>
+            {/*right column*/}
+            <div class="border-2 border-yellow-300 p-8 flex justify-center border-2 border-teal-400">
+
+            </div>
+        </div>
         
     </section>
 }
